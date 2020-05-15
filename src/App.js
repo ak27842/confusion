87728c366d98069156fbb52436daaefe66c60afa
Aditya Kaushik <adityaskaushik1@gmail.com>
2020-05-15 13:27:36 -0400

    Menu Component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent.js';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color = "primary">
         <div className = "container">
           <NavbarBrand href="/"> Ristorante con Fusion </NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
